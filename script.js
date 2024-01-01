1997f08545978182a0273e430481d5a4814e377c
l08sw4 <150978908+l08sw4@users.noreply.github.com>
2024-01-01 19:25:26 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -26,29 +26,3 @@ function includeHTML() {
   }
 };
 
-
-const label = document.querySelectorAll('.label');
-label.forEach(function (lb) {
-  lb.addEventListener('click', e => {
-    let optionList = lb.nextElementSibling;
-    let optionItems = optionList.querySelectorAll('.optionItem');
-    clickLabel(lb, optionItems);
-  })
-});
-const clickLabel = (lb, optionItems) => {
-  if (lb.parentNode.classList.contains('active')) {
-    lb.parentNode.classList.remove('active');
-    optionItems.forEach((opt) => {
-      opt.removeEventListener('click', () => {
-        handleSelect(lb, opt)
-      })
-    })
-  } else {
-    lb.parentNode.classList.add('active');
-    optionItems.forEach((opt) => {
-      opt.addEventListener('click', () => {
-        handleSelect(lb, opt)
-      })
-    })
-  }
-}
